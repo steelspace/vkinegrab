fd7398fb6ec5275d1c0a3cb7bbe3c98379688983
Petr Valach <petr.valach@hotmail.com>
2026-02-02 11:34:25 +0100

    a

--- Services/MovieCollectorService.cs
+++ Services/MovieCollectorService.cs
@@ -1,7 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using vkinegrab.Models;
 using vkinegrab.Services.Csfd;
 
--- Services/SchedulesStoreService.cs
+++ Services/SchedulesStoreService.cs
@@ -1,8 +1,4 @@
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using vkinegrab.Models;
-using vkinegrab.Services.Csfd;
 
 namespace vkinegrab.Services;
 
